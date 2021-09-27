457afaec1881f374cd1b021a39d005477e22ab4c
Pei-Hsun-onGithub <83334995+Pei-Hsun-onGithub@users.noreply.github.com>
2021-09-27 14:24:57 +0800

    整理目錄

---
+++ src/com/OrderList/modle/OrderListDAOService.java
@@ -0,0 +1,54 @@
+package com.OrderList.modle;
+
+import java.util.List;
+import com.OrderList.modle.OrderListVO;
+
+public class OrderListDAOService {
+
+	private OrderListDAO_interface dao;
+
+	public OrderListDAOService() {
+		dao = new OrderListDAO();
+	}
+
+	public OrderListVO addOrderListDAO( Integer orderId, Integer mealId, String quantity, String unitPrice, String note) {
+		
+		OrderListVO orderListVO = new OrderListVO();
+		
+		orderListVO.setOrderId(orderId);
+		orderListVO.setMealId(mealId);
+		orderListVO.setQuantity(quantity);
+		orderListVO.setUnitPrice(quantity);
+		orderListVO.setNote(note);
+		dao.insert(orderListVO);
+		
+		return orderListVO;
+	}
+
+	public OrderListVO updateOrderListDAO( Integer orderId, Integer mealId, String quantity, String unitPrice, String note) {
+		
+		OrderListVO orderListVO = new OrderListVO();
+		
+		orderListVO.setOrderId(orderId);
+		orderListVO.setMealId(mealId);
+		orderListVO.setQuantity(quantity);
+		orderListVO.setUnitPrice(quantity);
+		orderListVO.setNote(note);
+		dao.update(orderListVO);
+		
+		return orderListVO;
+	}
+
+	public void deleteOrderListDAO(Integer orderListid) {
+		dao.delete(orderListid);
+	}
+
+	public OrderListVO getOrderListVAO(Integer orderListid) {
+		return dao.findByPK(orderListid);
+	}
+
+	public List<OrderListVO> getAll() {
+		return dao.getAll();
+	}
+
+}
