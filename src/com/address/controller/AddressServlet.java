cd93a82e813fdf81dfd2a7cd1774003c26d66688
Jackyeh01 <niceyoyo2@gmail.com>
2021-10-05 15:10:19 +0800

    10/05 更新進度

--- src/com/address/controller/AddressServlet.java
+++ src/com/address/controller/AddressServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 import com.address.model.*;
+import com.address.model.AddressVO;
 
 public class AddressServlet extends HttpServlet {
 
@@ -38,7 +39,7 @@ public class AddressServlet extends HttpServlet {
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
 					RequestDispatcher failureView = req
-							.getRequestDispatcher("/select_page.jsp");
+							.getRequestDispatcher("/address/select_page.jsp");
 					failureView.forward(req, res);
 					return;//程式中斷
 				}
@@ -52,7 +53,7 @@ public class AddressServlet extends HttpServlet {
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
 					RequestDispatcher failureView = req
-							.getRequestDispatcher("/select_page.jsp");
+							.getRequestDispatcher("/address/select_page.jsp");
 					failureView.forward(req, res);
 					return;//程式中斷
 				}
@@ -66,7 +67,7 @@ public class AddressServlet extends HttpServlet {
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
 					RequestDispatcher failureView = req
-							.getRequestDispatcher("/select_page.jsp");
+							.getRequestDispatcher("/address/select_page.jsp");
 					failureView.forward(req, res);
 					return;//程式中斷
 				}
@@ -81,7 +82,7 @@ public class AddressServlet extends HttpServlet {
 			} catch (Exception e) {
 				errorMsgs.add("無法取得資料:" + e.getMessage());
 				RequestDispatcher failureView = req
-						.getRequestDispatcher("/select_page.jsp");
+						.getRequestDispatcher("/address/select_page.jsp");
 				failureView.forward(req, res);
 			}
 		}
@@ -96,11 +97,11 @@ public class AddressServlet extends HttpServlet {
 			
 			try {
 				/***************************1.接收請求參數****************************************/
-				Integer deliveryAddId = new Integer(req.getParameter("deliveryAddId"));
+				Integer userId = new Integer(req.getParameter("userId"));
 				
 				/***************************2.開始查詢資料****************************************/
 				AddressService addressSvc = new AddressService();
-				AddressVO addressVO = addressSvc.getOneAddress(deliveryAddId);
+				AddressVO addressVO = addressSvc.getOneAddress(userId);
 								
 				/***************************3.查詢完成,準備轉交(Send the Success view)************/
 				req.setAttribute("addressVO", addressVO);         // 資料庫取出的addressVO物件,存入req
@@ -118,7 +119,7 @@ public class AddressServlet extends HttpServlet {
 		}
 		
 		
-		if ("update".equals(action)) { // 來自update_address_input.jsp的請求
+if ("update".equals(action)) { // 來自update_emp_input.jsp的請求
 			
 			List<String> errorMsgs = new LinkedList<String>();
 			// Store this set in the request scope, in case we need to
@@ -127,59 +128,55 @@ public class AddressServlet extends HttpServlet {
 		
 			try {
 				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
-				Integer deliveryAddId = new Integer(req.getParameter("deliveryAddId").trim());
+Integer deliveryAddId = new Integer(req.getParameter("deliveryAddId").trim());
 				
-//				String ename = req.getParameter("ename");
-//				String enameReg = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{2,10}$";
-//				if (ename == null || ename.trim().length() == 0) {
-//					errorMsgs.add("員工姓名: 請勿空白");
-//				} else if(!ename.trim().matches(enameReg)) { //以下練習正則(規)表示式(regular-expression)
-//					errorMsgs.add("員工姓名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間");
-//	            }
-				
-				String job = req.getParameter("job").trim();
-				if (job == null || job.trim().length() == 0) {
-					errorMsgs.add("職位請勿空白");
-				}	
-				
-//				java.sql.Date hiredate = null;
-//				try {
-//					hiredate = java.sql.Date.valueOf(req.getParameter("hiredate").trim());
-//				} catch (IllegalArgumentException e) {
-//					hiredate=new java.sql.Date(System.currentTimeMillis());
-//					errorMsgs.add("請輸入日期!");
-//				}
-
-				Double sal = null;
+			Integer userId = null;
 				try {
-					sal = new Double(req.getParameter("sal").trim());
+					userId = new Integer(req.getParameter("userId").trim());
 				} catch (NumberFormatException e) {
-					sal = 0.0;
-					errorMsgs.add("薪水請填數字.");
+					userId = 0;
+					errorMsgs.add("請填會員編號");
 				}
 
-				Double comm = null;
-				try {
-					comm = new Double(req.getParameter("comm").trim());
-				} catch (NumberFormatException e) {
-					comm = 0.0;
-					errorMsgs.add("獎金請填數字.");
-				}
-
-				Integer deptno = new Integer(req.getParameter("deptno").trim());
+				String customerName = req.getParameter("customerName");
+				String enameReg = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{2,10}$";
+				if (customerName == null || customerName.trim().length() == 0) {
+					errorMsgs.add("取餐人姓名: 請勿空白");
+				} else if(!customerName.trim().matches(enameReg)) { //以下練習正則(規)表示式(regular-expression)
+					errorMsgs.add("取餐人姓名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間");
+	            }
+				
+				String deliverPhone = req.getParameter("deliverPhone");
+				String deliverPhoneReg = "^09\\d{2}(\\d{6}|-\\d{3}-\\d{3})$";
+				if(!deliverPhone.trim().matches(deliverPhoneReg)) { //以下練習正則(規)表示式(regular-expression)
+					errorMsgs.add("");
+	            }
+				
+				String deliverAddress = req.getParameter("deliverAddress");
+				String AddressReg = "^[(\\u4e00-\\u9fa5)(a-zA-Z0-9)]{100}$";
+				if (deliverAddress == null || deliverAddress.trim().length() == 0) {
+					errorMsgs.add("地址: 請勿空白");
+	            } else if(!deliverAddress.trim().matches(AddressReg)) { //以下練習正則(規)表示式(regular-expression)
+					errorMsgs.add("地址: 只能是中、英文字母、數字 , 且長度必需在100之間");
+	            }
+				
+				String buildingName = req.getParameter("buildingName");
+				String note = req.getParameter("note");
+				Integer sta = new Integer(req.getParameter("sta").trim());
+				//因為是get get都是字串。數字回來要轉型，所以用new。
 
 				AddressVO addressVO = new AddressVO();
-				addressVO.setAddressno(addressno);
-				addressVO.setEname(ename);
-				addressVO.setJob(job);
-				addressVO.setHiredate(hiredate);
-				addressVO.setSal(sal);
-				addressVO.setComm(comm);
-				addressVO.setDeptno(deptno);
+				addressVO.setUserId(userId);
+				addressVO.setCustomerName(customerName);
+				addressVO.setDeliverPhone(deliverPhone);
+				addressVO.setDeliverAddress(deliverAddress);
+				addressVO.setBuildingName(buildingName);
+				addressVO.setNote(note);
+				addressVO.setSta(sta);
 
 				// Send the use back to the form, if there were errors
-				if (!errorMsgs.isAddressty()) {
-					req.setAttribute("addressVO", addressVO); // 含有輸入格式錯誤的addressVO物件,也存入req
+				if (!errorMsgs.isEmpty()) {
+					req.setAttribute("addressVO", addressVO); // 含有輸入格式錯誤的empVO物件,也存入req
 					RequestDispatcher failureView = req
 							.getRequestDispatcher("/address/update_address_input.jsp");
 					failureView.forward(req, res);
@@ -188,12 +185,12 @@ public class AddressServlet extends HttpServlet {
 				
 				/***************************2.開始修改資料*****************************************/
 				AddressService addressSvc = new AddressService();
-				addressVO = addressSvc.updateAddress(addressno, ename, job, hiredate, sal,comm, deptno);
+				addressVO = addressSvc.updateAddress(userId,customerName,deliverPhone,deliverAddress,buildingName,note,sta);
 				
 				/***************************3.修改完成,準備轉交(Send the Success view)*************/
-				req.setAttribute("addressVO", addressVO); // 資料庫update成功後,正確的的addressVO物件,存入req
+				req.setAttribute("addressVO", addressVO); // 資料庫update成功後,正確的的empVO物件,存入req
 				String url = "/address/listOneAddress.jsp";
-				RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交listOneAddress.jsp
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交listOneEmp.jsp
 				successView.forward(req, res);
 
 				/***************************其他可能的錯誤處理*************************************/
@@ -205,117 +202,117 @@ public class AddressServlet extends HttpServlet {
 			}
 		}
 
-        if ("insert".equals(action)) { // 來自addAddress.jsp的請求  
-			
-			List<String> errorMsgs = new LinkedList<String>();
-			// Store this set in the request scope, in case we need to
-			// send the ErrorPage view.
-			req.setAttribute("errorMsgs", errorMsgs);
-
-			try {
-				/***********************1.接收請求參數 - 輸入格式的錯誤處理*************************/
-				String ename = req.getParameter("ename");
-				String enameReg = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{2,10}$";
-				if (ename == null || ename.trim().length() == 0) {
-					errorMsgs.add("員工姓名: 請勿空白");
-				} else if(!ename.trim().matches(enameReg)) { //以下練習正則(規)表示式(regular-expression)
-					errorMsgs.add("員工姓名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間");
-	            }
-				
-				String job = req.getParameter("job").trim();
-				if (job == null || job.trim().length() == 0) {
-					errorMsgs.add("職位請勿空白");
-				}
-				
-				java.sql.Date hiredate = null;
-				try {
-					hiredate = java.sql.Date.valueOf(req.getParameter("hiredate").trim());
-				} catch (IllegalArgumentException e) {
-					hiredate=new java.sql.Date(System.currentTimeMillis());
-					errorMsgs.add("請輸入日期!");
-				}
-				
-				Double sal = null;
-				try {
-					sal = new Double(req.getParameter("sal").trim());
-				} catch (NumberFormatException e) {
-					sal = 0.0;
-					errorMsgs.add("薪水請填數字.");
-				}
-				
-				Double comm = null;
-				try {
-					comm = new Double(req.getParameter("comm").trim());
-				} catch (NumberFormatException e) {
-					comm = 0.0;
-					errorMsgs.add("獎金請填數字.");
-				}
-				
-				Integer deptno = new Integer(req.getParameter("deptno").trim());
-
-				AddressVO addressVO = new AddressVO();
-				addressVO.setEname(ename);
-				addressVO.setJob(job);
-				addressVO.setHiredate(hiredate);
-				addressVO.setSal(sal);
-				addressVO.setComm(comm);
-				addressVO.setDeptno(deptno);
-
-				// Send the use back to the form, if there were errors
-				if (!errorMsgs.isAddressty()) {
-					req.setAttribute("addressVO", addressVO); // 含有輸入格式錯誤的addressVO物件,也存入req
-					RequestDispatcher failureView = req
-							.getRequestDispatcher("/address/addAddress.jsp");
-					failureView.forward(req, res);
-					return;
-				}
-				
-				/***************************2.開始新增資料***************************************/
-				AddressService addressSvc = new AddressService();
-				addressVO = addressSvc.addAddress(ename, job, hiredate, sal, comm, deptno);
-				
-				/***************************3.新增完成,準備轉交(Send the Success view)***********/
-				String url = "/address/listAllAddress.jsp";
-				RequestDispatcher successView = req.getRequestDispatcher(url); // 新增成功後轉交listAllAddress.jsp
-				successView.forward(req, res);				
-				
-				/***************************其他可能的錯誤處理**********************************/
-			} catch (Exception e) {
-				errorMsgs.add(e.getMessage());
-				RequestDispatcher failureView = req
-						.getRequestDispatcher("/address/addAddress.jsp");
-				failureView.forward(req, res);
-			}
-		}
-		
-		
-		if ("delete".equals(action)) { // 來自listAllAddress.jsp
-
-			List<String> errorMsgs = new LinkedList<String>();
-			// Store this set in the request scope, in case we need to
-			// send the ErrorPage view.
-			req.setAttribute("errorMsgs", errorMsgs);
-	
-			try {
-				/***************************1.接收請求參數***************************************/
-				Integer addressno = new Integer(req.getParameter("addressno"));
-				
-				/***************************2.開始刪除資料***************************************/
-				AddressService addressSvc = new AddressService();
-				addressSvc.deleteAddress(addressno);
-				
-				/***************************3.刪除完成,準備轉交(Send the Success view)***********/								
-				String url = "/address/listAllAddress.jsp";
-				RequestDispatcher successView = req.getRequestDispatcher(url);// 刪除成功後,轉交回送出刪除的來源網頁
-				successView.forward(req, res);
-				
-				/***************************其他可能的錯誤處理**********************************/
-			} catch (Exception e) {
-				errorMsgs.add("刪除資料失敗:"+e.getMessage());
-				RequestDispatcher failureView = req
-						.getRequestDispatcher("/address/listAllAddress.jsp");
-				failureView.forward(req, res);
-			}
-		}
+//        if ("insert".equals(action)) { // 來自addEmp.jsp的請求  
+//			
+//			List<String> errorMsgs = new LinkedList<String>();
+//			// Store this set in the request scope, in case we need to
+//			// send the ErrorPage view.
+//			req.setAttribute("errorMsgs", errorMsgs);
+//
+//			try {
+//				/***********************1.接收請求參數 - 輸入格式的錯誤處理*************************/
+//String ename = req.getParameter("ename");
+//				String enameReg = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{2,10}$";
+//				if (ename == null || ename.trim().length() == 0) {
+//					errorMsgs.add("員工姓名: 請勿空白");
+//				} else if(!ename.trim().matches(enameReg)) { //以下練習正則(規)表示式(regular-expression)
+//					errorMsgs.add("員工姓名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間");
+//	            }
+//				
+//String job = req.getParameter("job").trim();
+//				if (job == null || job.trim().length() == 0) {
+//					errorMsgs.add("職位請勿空白");
+//				}
+//				
+//				java.sql.Date hiredate = null;
+//				try {
+//hiredate = java.sql.Date.valueOf(req.getParameter("hiredate").trim());
+//				} catch (IllegalArgumentException e) {
+//					hiredate=new java.sql.Date(System.currentTimeMillis());
+//					errorMsgs.add("請輸入日期!");
+//				}
+//				
+//				Double sal = null;
+//				try {
+//sal = new Double(req.getParameter("sal").trim());
+//				} catch (NumberFormatException e) {
+//					sal = 0.0;
+//					errorMsgs.add("薪水請填數字.");
+//				}
+//				
+//				Double comm = null;
+//				try {
+//comm = new Double(req.getParameter("comm").trim());
+//				} catch (NumberFormatException e) {
+//					comm = 0.0;
+//					errorMsgs.add("獎金請填數字.");
+//				}
+//				
+//Integer deptno = new Integer(req.getParameter("deptno").trim());
+//
+//				EmpVO empVO = new EmpVO();
+//				empVO.setEname(ename);
+//				empVO.setJob(job);
+//				empVO.setHiredate(hiredate);
+//				empVO.setSal(sal);
+//				empVO.setComm(comm);
+//				empVO.setDeptno(deptno);
+//
+//				// Send the use back to the form, if there were errors
+//				if (!errorMsgs.isEmpty()) {
+//					req.setAttribute("empVO", empVO); // 含有輸入格式錯誤的empVO物件,也存入req
+//					RequestDispatcher failureView = req
+//							.getRequestDispatcher("/emp/addEmp.jsp");
+//					failureView.forward(req, res);
+//					return;
+//				}
+//				
+//				/***************************2.開始新增資料***************************************/
+//				EmpService empSvc = new EmpService();
+//				empVO = empSvc.addEmp(ename, job, hiredate, sal, comm, deptno);
+//				
+//				/***************************3.新增完成,準備轉交(Send the Success view)***********/
+//				String url = "/emp/listAllEmp.jsp";
+//				RequestDispatcher successView = req.getRequestDispatcher(url); // 新增成功後轉交listAllEmp.jsp
+//				successView.forward(req, res);				
+//				
+//				/***************************其他可能的錯誤處理**********************************/
+//			} catch (Exception e) {
+//				errorMsgs.add(e.getMessage());
+//				RequestDispatcher failureView = req
+//						.getRequestDispatcher("/emp/addEmp.jsp");
+//				failureView.forward(req, res);
+//			}
+//		}
+//		
+//		
+//		if ("delete".equals(action)) { // 來自listAllEmp.jsp
+//
+//			List<String> errorMsgs = new LinkedList<String>();
+//			// Store this set in the request scope, in case we need to
+//			// send the ErrorPage view.
+//			req.setAttribute("errorMsgs", errorMsgs);
+//	
+//			try {
+//				/***************************1.接收請求參數***************************************/
+//				Integer empno = new Integer(req.getParameter("empno"));
+//				
+//				/***************************2.開始刪除資料***************************************/
+//				EmpService empSvc = new EmpService();
+//				empSvc.deleteEmp(empno);
+//				
+//				/***************************3.刪除完成,準備轉交(Send the Success view)***********/								
+//				String url = "/emp/listAllEmp.jsp";
+//				RequestDispatcher successView = req.getRequestDispatcher(url);// 刪除成功後,轉交回送出刪除的來源網頁
+//				successView.forward(req, res);
+//				
+//				/***************************其他可能的錯誤處理**********************************/
+//			} catch (Exception e) {
+//				errorMsgs.add("刪除資料失敗:"+e.getMessage());
+//				RequestDispatcher failureView = req
+//						.getRequestDispatcher("/emp/listAllEmp.jsp");
+//				failureView.forward(req, res);
+//			}
+//		}
 	}
 }
